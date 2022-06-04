ad75ecaa9bf03a27bdaa566ae359d652f7fd2924
trapell001 <56587525+trapell001@users.noreply.github.com>
2022-06-04 22:09:28 +0700

    Create SinhVien.java

---
+++ Fresher_Collection_MiniEx_01/src/com/example/SinhVien.java
@@ -0,0 +1,88 @@
+package com.example;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class SinhVien {
+    private String mssv;
+    private String ten;
+
+    private Set<Diem> monDaHoc = new HashSet<Diem>();
+
+    public SinhVien(String mssv, String ten, Set<Diem> monDaHoc) {
+        this.mssv = mssv;
+        this.ten = ten;
+        this.monDaHoc = monDaHoc;
+    }
+
+    public String getMssv() {
+        return mssv;
+    }
+
+    public void setMssv(String mssv) {
+        this.mssv = mssv;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public Set<Diem> getMonDaHoc() {
+        return monDaHoc;
+    }
+
+    public void setMonDaHoc(Set<Diem> monDaHoc) {
+        this.monDaHoc = monDaHoc;
+    }
+
+    public boolean themDiem(Diem diemMoi) {
+        return this.monDaHoc.add(diemMoi);
+    }
+
+    //TODO Cau 1
+    public double tinhDiemTrungBinh() {
+        //Giong nhu cach tinh hien tai cua truong
+        //...
+        int tongDiem=monDaHoc.stream().mapToInt(Diem::tongDiem).sum();
+        int tongTC=monDaHoc.stream().mapToInt(Diem::tongTC).sum();
+
+        return (double) tongDiem/tongTC;
+    }
+
+
+    //TODO Cau 2
+    public String bangDiem() {
+    /*
+     MSSV : 0203044
+     Ten  : Nguyen Van A
+     Danh Sach Diem
+     STT  Ten Mon             Diem       So Tin Chi
+     1    Cau Truc Du Lieu 1  8          3
+     2    Cau Truc Du Lieu 2  8          3
+     Diem Trung Binh   8.0
+    */
+        //...
+        //StringBuilder
+        return null;
+    }
+
+
+    //TODO Cau 3
+    public String xepLoai() {
+    /*
+    Quy tac xep loai nhu sau
+        DiemTB < 5 -> YEU
+        DiemTB >= 5 va DiemTB < 6 -> TB
+        DiemTB >= 6 va DiemTB < 7 -> TB-KHA
+        DiemTB >= 7 va DiemTB < 8 -> KHA
+        DiemTB >= 8 -> GIOI
+    */
+
+        //...
+        return null;
+    }
+}
