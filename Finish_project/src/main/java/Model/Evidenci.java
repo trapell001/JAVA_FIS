ca4ccbc541474d62076963718d4bb957116408cc
trapell001 <56587525+trapell001@users.noreply.github.com>
2022-06-04 21:58:57 +0700

    Create Evidenci.java

---
+++ Finish_project/src/main/java/Model/Evidenci.java
@@ -0,0 +1,14 @@
+package Model;
+
+import Util.CaseStatus;
+import Util.Storage;
+
+public class Evidenci extends AbstractEntity{
+    private CaseStatus caseStatus;
+    private Storage storage;
+    private String number;
+    private String itemName;
+    private String notes;
+    private boolean archived;
+
+}
