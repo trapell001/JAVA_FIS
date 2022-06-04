cd96d4c92d5abbca6956d55b4fdb3be035fbce2c
trapell001 <56587525+trapell001@users.noreply.github.com>
2022-06-04 22:02:42 +0700

    Create Form.java

---
+++ LAB2-1-2/src/Component/Form.java
@@ -0,0 +1,15 @@
+package Component;
+
+import java.awt.*;
+import java.util.List;
+
+public class Form {
+    private List<Component> components;
+
+    public void addComponent(Component component) {
+    }
+
+    public boolean validateForm() {
+        return true;
+    }
+}
