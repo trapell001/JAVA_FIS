e54b80eed1212302030b15e89f42532444f5cc0a
trapell001 <56587525+trapell001@users.noreply.github.com>
2022-06-19 17:18:42 +0700

    a

--- fis-test02/src/main/Test/Test/OrderServiceImplTest.java
+++ fis-test02/src/main/Test/Test/OrderServiceImplTest.java
@@ -1,13 +1,24 @@
 package Test;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import vn.fis.training.ordermanagement.domain.Order;
+import vn.fis.training.ordermanagement.domain.OrderItem;
+import vn.fis.training.ordermanagement.domain.OrderStatus;
+import vn.fis.training.ordermanagement.repository.OrderRepository;
+import vn.fis.training.ordermanagement.service.OrderService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceImplTest {
-
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private OrderRepository orderRepository;
     @Test
     void createOrder() {
+        Order order = new Order();
+        orderService.createOrder(order);
     }
 
     @Test
@@ -16,10 +27,15 @@ class OrderServiceImplTest {
 
     @Test
     void removeOrderItem() {
+        Order order = orderService.findOrdersById(1l);
+        OrderItem orderItem = order.getOrderItems().get(1);
+        order.getOrderItems().remove(orderItem);
     }
 
     @Test
     void updateOrderStatus() {
+        Order order = orderService.findOrdersById(1l);
+        orderService.updateOrderStatus(order, OrderStatus.WAITING_APPROVAL);
     }
 
     @Test
